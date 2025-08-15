5cb37d2abacda2d2792e5953caf8adf8f7a8c52d
elif-programing <elifcerenturkyilmaz@gmail.com>
2025-08-15 14:10:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['🫀', '⚡️', '🐍', '✨', '💫', '🫶🏼','🐍','❤️'],
+    }).then(() => jsConfetti.addConfetti ())
+    .then(() => jsConfetti.addConfetti())
+    
+})
